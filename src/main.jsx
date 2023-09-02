b9ecce51e379724568997378c88e36160a3734a8
omerfkgoze <farukucukgoze@gmail.com>
2023-09-02 18:37:12 +0200

    updated

--- src/main.jsx
+++ src/main.jsx
@@ -132,12 +132,12 @@ function Footer() {
   );
 }
 
-function Order({ closeHour, openHour, istediğimizTanımlama }) {
+function Order({ closeHour, openHour }) {
   return (
     <div className="order">
       <p>
         We're open from {openHour}:00 to {closeHour}:00. Come visit us or order
-        online. {istediğimizTanımlama}
+        online.
       </p>
       <button className="btn">Order now!</button>
     </div>
